ec664de2cd78af6950d14f6d61115311e4d5c866
Mathieu Baurin <mathieubaurin@MacBook-Pro-de-Mathieu.local>
2021-10-25 01:18:22 +0200

    write env variables when ->key

--- srcs/parser.c
+++ srcs/parser.c
@@ -67,10 +67,147 @@ void	fill_cmd_array(t_shell *shell, int *each_cmd_lenght)
 	}
 }
 
+int		ft_is_uppercase(char c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
+
+char	*get_env_key(t_shell *shell)
+{
+	int		i;
+	int		j;
+	int		count;
+	int		milestone;
+	char	*tmp;
+
+	i = 0;
+	count = 0;
+	milestone = 0;
+	while (shell->command_line_clean[i])
+	{
+		if (shell->command_line_clean[i] == '$')
+		{
+			j = i + 1;
+			milestone = i + 1;
+			while (ft_is_uppercase(shell->command_line_clean[j]) || shell->command_line_clean[j] == '_')
+			{
+				count++;
+				j++;
+			}
+			break ;
+		}
+		i++;
+	}
+	tmp = ft_substr(shell->command_line_clean, milestone, count);
+	return (tmp);
+}
+
+int		next_word_env_key(t_shell *shell)
+{
+	char	*tmp;
+	t_list  *lst;
+	t_env	*env;
+
+	tmp = get_env_key(shell);
+	lst = shell->env_vars;
+    while (lst)
+	{
+		env = (t_env*)lst->content;
+		if (ft_strcmp(env->key, tmp) == 0)
+            return (1);
+		lst = lst->next;
+	}
+	return (0);
+}
+
+int		have_env_variables(t_shell *shell)
+{
+	int		i;
+
+	i = 0;
+	while (shell->command_line_clean[i])
+	{
+		if (shell->command_line_clean[i] == '$' && next_word_env_key(shell))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*get_replacement_env_key(t_shell *shell)
+{
+	char	*tmp;
+	t_list  *lst;
+	t_env	*env;
+
+	tmp = get_env_key(shell);
+	lst = shell->env_vars;
+    while (lst)
+	{
+		env = (t_env*)lst->content;
+		if (ft_strcmp(env->key, tmp) == 0)
+            return (env->value);
+		lst = lst->next;
+	}
+	return (NULL);
+}
+
+char	*replace_key_to_value_env(t_shell *shell)
+{
+	int		i;
+	int		pos;
+	int		lenght_rest;
+	char	*tmp1;
+	char	*tmp2;
+	char	*tmp3;
+	char	*result;
+	char	*to_replace;
+
+	i = 0;
+	while (shell->command_line_clean[i])
+	{
+		if (shell->command_line_clean[i] == '$')
+			break ;
+		i++;
+	}
+	tmp1 = ft_substr(shell->command_line_clean, 0, i);
+	to_replace = get_replacement_env_key(shell);
+	tmp2 = ft_strjoin(tmp1, to_replace);
+	i = 0;
+	while (shell->command_line_clean[i])
+	{
+		if (ft_is_uppercase(shell->command_line_clean[i]) && shell->command_line_clean[i + 1] == ' ')
+			break ;
+		i++;
+	}
+	pos = i + 1;
+	lenght_rest = 0;
+	while (shell->command_line_clean[i])
+	{
+		lenght_rest++;
+		i++;
+	}
+	tmp3 = ft_substr(shell->command_line_clean, pos, lenght_rest);
+	result = ft_strjoin(tmp2, tmp3);
+	return (result);
+}
+
+void	resolve_replace_key_to_value_env(t_shell *shell)
+{
+	do
+	{
+		shell->command_line_clean = replace_key_to_value_env(shell);
+	} while (have_env_variables(shell));
+}
+
 void	parse_command(t_shell *shell)
 {
 	int		*each_cmd_lenght;
 
+	if (have_env_variables(shell))
+		resolve_replace_key_to_value_env(shell);
 	shell->command_line_clean = check_if_quotes(shell->command_line_clean);
 	if (shell->command_line_clean == NULL)
 		return ;
