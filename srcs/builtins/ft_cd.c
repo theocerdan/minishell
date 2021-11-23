38ed3b32d9d305541862d8564493ce75d3957348
theocerdan <cerdantheo.contact@gmail.com>
2021-11-23 14:55:17 +0100

    cd return value

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -38,6 +38,7 @@ void    ft_cd(t_shell *shell, char *each_cmd)
     char    *old_pwdv;
     char    *target_pwdv;
     int     ret;
+
     (void)(each_cmd);
     (void)(shell);
     arg_len = 0;
@@ -52,6 +53,8 @@ void    ft_cd(t_shell *shell, char *each_cmd)
     ret = chdir(target_pwdv);
     if (ret == 0)
         update_oldpwd_pwd_vars(shell, old_pwdv);
-    else
+    else {
+        shell->error_return = 1;
         printf("cd: %s: No such file or directory\n", target_pwdv);
+    }
 }
